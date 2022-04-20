efff5e7942e52726284f953926a8a2cf0490a950
Josimar Jose Nepomuceno <josimar.n2007@gmail.com>
2022-04-20 18:20:57 -0300

    Criação do projeto

---
+++ src/main/java/com/gama/academy/FolhaDePagamentoApplication.java
@@ -0,0 +1,13 @@
+package com.gama.academy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FolhaDePagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FolhaDePagamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gama/academy/FolhaDePagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gama.academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolhaDePagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
